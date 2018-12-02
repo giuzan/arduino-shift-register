fe4ac82a8209da5bb8a0d6e9822f2ae00f6439df
Giuliano Zannetti <giuzan91@gmail.com>
2018-12-02 19:15:51 +0100

    add comment

--- shift_register.cpp
+++ shift_register.cpp
@@ -1,5 +1,5 @@
 /*
-  74HC595
+  74HC595 (only one)
          _____
    Q1  -|°    |-  VCC
    Q2  -|     |-  Q0
@@ -8,6 +8,30 @@
    Q5  -|     |-  ST_CP (latch pin)
    Q6  -|     |-  SH_CP (clock pin)
    Q7  -|     |-  /MR (master reset pin)
+  GND  -|_____|-  Q7'
+*/
+
+/*
+  74HC595 (two or more)
+
+         _____
+   Q1  -|°    |-  VCC
+   Q2  -|     |-  Q0
+   Q3  -|     |-  DS (data pin)
+   Q4  -|     |-  /OE <--> /OE(2)
+   Q5  -|     |-  ST_CP (latch pin) <--> ST_CP(2)
+   Q6  -|     |-  SH_CP (clock pin) <--> SH_CP(2)
+   Q7  -|     |-  /MR (master reset pin) <--> /MR(2)
+  GND  -|_____|-  Q7' <--> DS(2)
+
+         _____
+   Q1  -|°    |-  VCC
+   Q2  -|     |-  Q0
+   Q3  -|     |-  DS <--> Q7'(1)
+   Q4  -|     |-  /OE <--> /OE(1)
+   Q5  -|     |-  ST_CP <--> ST_CP(1)
+   Q6  -|     |-  SH_CP <--> SH_CP(1)
+   Q7  -|     |-  /MR <--> /MR(1)
   GND  -|_____|-  Q7'
 */
 
